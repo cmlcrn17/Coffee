5503020ed3b30262649e107f4dfeb0c756169c8c
Ceren <cemilecerenerdem@gmail.com>
2020-11-14 21:40:56 +0300

    Initial Commit

---
+++ Coffe/CoffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeApp.swift
+//  Coffe
+//
+//  Created by Ceren TAŞSIN on 14.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct CoffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
